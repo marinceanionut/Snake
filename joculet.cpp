c426d4c9f937425097d15008bd9a6f72f7cae853
marinceanionut <marincean.ioan@gmail.com>
2017-01-10 14:22:02 +0200

    joculet.cpp

--- joculet.cpp
+++ joculet.cpp
@@ -5,8 +5,17 @@ using namespace std;
 
 sf::VideoMode VMode(840, 564, 32);
 sf::RenderWindow win(VMode, "Snake");
+struct coordonate
+{
+    int x, y;
+};
+int Map[1000][1000];
+int prim,ultim;
+coordonate worm[200000];
+coordonate mar;
 
 void play_mode();
+bool single_player();
 
 int main()
 {
@@ -73,7 +82,7 @@ int main()
 void play_mode()
 {
     sf::Texture mod_joc;
-    mod_joc.loadFromFile("play_modes.png");
+    mod_joc.loadFromFile("play_mode.png");
     sf::Sprite sprite(mod_joc);
     while (win.isOpen() )
     {
@@ -104,5 +113,72 @@ void play_mode()
         win.draw(sprite);
         win.display();
 
+    }
+}
+bool single_player()
+{
+    int dir=1, i, prim, ultim;
+    char nume[]="head0.png";
+    coordonate nou;
+    sf::Texture background;
+    background.loadFromFile("BACKGROUND.jpg");
+    sf::Sprite sprite1(background);
+    sf::Clock clock;
+    sf::Texture pen;
+    pen.loadFromFile("mar.png");
+    sf::Sprite apple(pen);
+    sf::Texture body;
+    body.loadFromFile("interior_segment.png");
+    sf::Sprite segm(body);
+    sf::Texture head;
+
+    coordonate d[10];
+
+    d[0].x=0; d[1].x=1; d[2].x=0; d[3].x=-1;
+    d[0].y=-1; d[1].y=0; d[2].y=1; d[3].y=0;
+
+
+    mar.x=10;
+    mar.y=10;
+    prim=0;
+    ultim=1;
+    nou.x=12;
+    nou.y=5;
+    worm[0]=nou;
+    nou.x=13;
+    nou.y=5;
+    worm[1]=nou;
+    Map[5][13]=Map[5][12]=1;
+
+    while(win.isOpen())
+    {
+
+        sf::Event event;
+        while (win.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                //win.close();
+                return 0;
+            }
+            else if(event.type == sf::Event::KeyPressed)
+            {
+                if(event.key.code == sf::Keyboard::Up && dir!=2)
+                    dir=0;
+                else if(event.key.code == sf::Keyboard::Down && dir!=0)
+                    dir=2;
+                else if(event.key.code == sf::Keyboard::Right && dir!=3)
+                    dir=1;
+                else if(event.key.code == sf::Keyboard::Left && dir!=1)
+                    dir=3;
+
+            }
+
+
+        }
+        if(clock.getElapsedTime().asMilliseconds()>=75)
+        {
+            clock.restart();
+        }
     }
 }
