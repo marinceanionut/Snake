4666c8743f9b48e0b050ec4b5a2fcceaa9b38c5f
marinceanionut <marincean.ioan@gmail.com>
2017-01-11 20:26:26 +0200

    --all

--- joculet.cpp
+++ joculet.cpp
@@ -10,12 +10,13 @@ struct coordonate
     int x, y;
 };
 int Map[1000][1000];
-int prim,ultim;
-coordonate worm[200000];
+int prim, ultim, prim_second, ultim_second;
+coordonate worm[200000], worm_second[200000];
 coordonate mar;
 
 void play_mode();
 bool single_player();
+bool multiplayer();
 
 int main()
 {
@@ -104,7 +105,7 @@ void play_mode()
                 }
                 else if( ( 194<event.mouseButton.x && event.mouseButton.x<623 ) && ( 356<event.mouseButton.y && event.mouseButton.y<393 ) )
                 {
-                    cout<<"Player vs player mode.\n";
+                    if(!multiplayer()) win.close();
                 }
             }
         }
@@ -241,4 +242,215 @@ bool single_player()
 
     return 0;
 
+}
+
+
+bool multiplayer ()
+{
+    int dir=1, dir_second=1, i, prim, ultim, prim_second, ultim_second;
+    char nume[]="head0.png";
+    int lg=0;
+    coordonate nou;
+    sf::Texture background;
+    background.loadFromFile("BACKGROUND.jpg");
+    sf::Sprite sprite1(background);
+    sf::Clock clock;
+    sf::Texture pen;
+    pen.loadFromFile("mar.png");
+    sf::Sprite apple(pen);
+    sf::Texture body;
+    body.loadFromFile("interior_segment.png");
+    sf::Sprite segm(body);
+    sf::Texture head;
+
+    coordonate d[10];
+
+    d[0].x=0; d[1].x=1; d[2].x=0; d[3].x=-1;
+    d[0].y=-1; d[1].y=0; d[2].y=1; d[3].y=0;
+
+
+    //adaug corpul
+    mar.x=10;
+    mar.y=10;
+
+    prim=0;
+    ultim=1;
+    nou.x=12;
+    nou.y=5;
+    worm[0]=nou;
+    nou.x=13;
+    nou.y=5;
+    worm[1]=nou;
+    Map[5][13]=Map[5][12]=1;
+
+
+    prim_second=0;
+    ultim_second=1;
+    nou.x=12;
+    nou.y=3;
+    worm_second[0]=nou;
+    nou.x=13;
+    nou.y=3;
+    worm_second[1]=nou;
+    Map[3][13]=Map[3][12]=2;
+
+    while(win.isOpen())
+    {
+
+        sf::Event event;
+        while (win.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                //win.close();
+                return 0;
+            }
+            else if(event.type == sf::Event::KeyPressed)
+            {
+                if(event.key.code == sf::Keyboard::Up && dir!=2)
+                    dir=0;
+                else if(event.key.code == sf::Keyboard::Down && dir!=0)
+                    dir=2;
+                else if(event.key.code == sf::Keyboard::Right && dir!=3)
+                    dir=1;
+                else if(event.key.code == sf::Keyboard::Left && dir!=1)
+                    dir=3;
+                else if(event.key.code == sf::Keyboard::W && dir_second!=2)
+                    dir_second=0;
+                else if(event.key.code == sf::Keyboard::S && dir_second!=0)
+                    dir_second=2;
+                else if(event.key.code == sf::Keyboard::D && dir_second!=3)
+                    dir_second=1;
+                else if(event.key.code == sf::Keyboard::A && dir_second!=1)
+                    dir_second=3;
+            }
+
+
+        }
+
+
+        //delay
+
+        if(clock.getElapsedTime().asMilliseconds()>=75)
+        {
+            ///primul sarpe
+            clock.restart();
+
+            nou.x=worm[ultim].x+d[dir].x;
+            nou.y=worm[ultim].y+d[dir].y;
+
+            if(nou.x==-1) nou.x=21;
+            else if (nou.x==21 ) nou.x=0;
+
+            if(nou.y==-1) nou.y=14;
+            else if (nou.y==14) nou.y=0;
+
+            worm[++ultim]=nou;
+
+            if(Map[worm[ultim].y][worm[ultim].x]==1)
+            {
+                //game over
+                return 0;
+
+            }
+            else Map[worm[ultim].y][worm[ultim].x]=1;
+
+            if(worm[ultim].x==mar.x && worm[ultim].y==mar.y)
+            {
+
+                do{
+                    mar.x = rand() % 20 + 1;
+                    mar.y = rand() % 13 + 1;
+                }
+                while ( Map[mar.y][mar.x]);
+            }
+            else
+            {
+                Map[ worm[prim].y ] [worm[prim].x ]=0;
+                ++prim;
+            }
+
+            ///al doilea sarpe
+
+            nou.x=worm_second[ultim_second].x+d[dir_second].x;
+            nou.y=worm_second[ultim_second].y+d[dir_second].y;
+
+            if(nou.x==-1) nou.x=21;
+            else if (nou.x==21 ) nou.x=0;
+
+            if(nou.y==-1) nou.y=14;
+            else if (nou.y==14) nou.y=0;
+
+            worm_second[++ultim_second]=nou;
+
+            if(Map[worm_second[ultim_second].y][worm_second[ultim_second].x]==2)
+            {
+                //game over
+                return 0;
+
+            }
+            else Map[worm_second[ultim_second].y][worm_second[ultim_second].x]=2;
+
+
+            if(worm_second[ultim_second].x==mar.x && worm_second[ultim_second].y==mar.y)
+            {
+
+                do{
+                    mar.x = rand() % 20 + 1;
+                    mar.y = rand() % 13 + 1;
+                }
+                while ( Map[mar.y][mar.x]);
+            }
+            else
+            {
+                Map[ worm_second[prim_second].y ] [worm_second[prim_second].x ]=0;
+                ++prim_second;
+            }
+
+        }
+
+        ///primul sarpe
+
+        nume[4]=dir+'0';
+        head.loadFromFile(nume);
+        sf::Sprite capet(head);
+        win.clear();
+        win.draw(sprite1);
+        apple.setPosition(40*mar.x, 40*mar.y);
+        win.draw(apple);
+        //desenez rama
+
+
+        for(i=prim; i<ultim; ++i)
+        {
+            segm.setPosition(40*worm[i].x, 40*worm[i].y);
+            win.draw(segm);
+        }
+
+        capet.setPosition(40*worm[ultim].x, 40*worm[ultim].y);
+        win.draw(capet);
+
+
+        ///al doilea sarpe
+
+        nume[4]=dir_second+'0';
+        head.loadFromFile(nume);
+        sf::Sprite capet2(head);
+        //desenez rama
+
+
+        for(i=prim_second; i<ultim_second; ++i)
+        {
+            segm.setPosition(40*worm_second[i].x, 40*worm_second[i].y);
+            win.draw(segm);
+        }
+
+        capet2.setPosition(40*worm_second[ultim_second].x, 40*worm_second[ultim_second].y);
+        win.draw(capet2);
+
+
+
+        win.display();
+    }
+    return 1;
 }
